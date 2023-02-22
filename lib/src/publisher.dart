1a80c09e78403ff243fd3aa457f2d5244b635276
SUZUKI Tetsuya <tetsuya.suzuki@gmail.com>
2023-02-23 05:07:56 +0900

    impl

--- lib/src/publisher.dart
+++ lib/src/publisher.dart
@@ -1,36 +1,58 @@
 import 'package:flutter/foundation.dart';
 
-class Cancellable {
+abstract class Cancellable {
+  void cancel();
+}
+
+class AnyCancellable implements Cancellable {
   @protected
-  const Cancellable(this.onCancelled);
+  const AnyCancellable(this._onCancelled);
 
-  final void Function() onCancelled;
+  final void Function() _onCancelled;
 
+  @override
   void cancel() {
-    onCancelled();
+    _onCancelled();
   }
 }
 
-class Publisher<Output, Failure extends Error> {
-  late final _subscribers = <dynamic, Cancellable>{};
+abstract class Publisher<Output, Failure extends Error> {
+  void receive<S extends Subscriber>(S subscriber);
+  Cancellable subscribe<S extends Subscriber>(S subscriber);
+  void assign(Publisher<Output, Failure> publisher);
+  Cancellable sink({
+    required void Function(Output) onReceive,
+    required void Function(Failure)? onFailure,
+  });
+}
 
-  void receive<S>(S subscriber) {
-    if (!_subscribers.containsKey(subscriber)) {
-      // TODO
-      _subscribers[subscriber] = Cancellable(() {
+class AnyPublisher<Output, Failure extends Error>
+    implements Publisher<Output, Failure> {
+  late final _subscribers = <Subscriber, Cancellable>{};
+
+  @override
+  void receive<S extends Subscriber>(S subscriber) {
+    subscribe(subscriber);
+  }
+
+  @override
+  Cancellable subscribe<S extends Subscriber>(S subscriber) {
+    Cancellable? cancel = _subscribers[subscriber];
+    if (cancel == null) {
+      cancel = AnyCancellable(() {
         _subscribers.remove(subscriber);
       });
+      _subscribers[subscriber] = cancel;
     }
+    return cancel;
   }
 
-  Cancellable subscribe<S>() {
-    throw UnimplementedError();
-  }
-
+  @override
   void assign(Publisher<Output, Failure> publisher) {
     throw UnimplementedError();
   }
 
+  @override
   Cancellable sink({
     required void Function(Output) onReceive,
     required void Function(Failure)? onFailure,
@@ -39,17 +61,79 @@ class Publisher<Output, Failure extends Error> {
   }
 }
 
-class Subscriber<Output, Failure extends Error>
-    extends Publisher<Output, Failure> {
-  void send(Output? value) {
-    throw UnimplementedError();
+abstract class Subject<Output, Failure extends Error>
+    implements Publisher<Output, Failure> {
+  void send(Output? value);
+  void request(Subscription subscription);
+  void finish();
+  void failure(Failure failure);
+}
+
+// TODO
+abstract class Subscription {}
+
+abstract class Subscriber<Output, Failure extends Error>
+    implements Publisher<Output, Failure> {
+  void send(Output? value);
+  void fail(Failure value);
+  void finish();
+}
+
+class Sink<Output, Failure extends Error>
+    implements Subscriber<Output, Failure>, Cancellable {
+  @protected
+  Sink({
+    required void Function(Output) onReceive,
+    required void Function(Failure)? onFailure,
+  }) {
+    _onReceive = onReceive;
+    _onFailure = onFailure;
   }
 
+  late void Function(Output) _onReceive;
+  void Function(Failure)? _onFailure;
+
+  @override
+  void assign(Publisher<Output, Failure> publisher) {
+    // TODO: implement assign
+  }
+
+  @override
+  void cancel() {
+    // TODO: implement cancel
+  }
+
+  @override
   void fail(Failure value) {
+    // TODO: implement fail
+  }
+
+  @override
+  void finish() {
+    // TODO: implement finish
+  }
+
+  @override
+  void receive<S extends Subscriber<dynamic, Error>>(S subscriber) {
+    // TODO: implement receive
+  }
+
+  @override
+  void send(Output? value) {
+    // TODO: implement send
+  }
+
+  @override
+  Cancellable sink(
+      {required void Function(Output p1) onReceive,
+      required void Function(Failure p1)? onFailure}) {
+    // TODO: implement sink
     throw UnimplementedError();
   }
 
-  void finish() {
+  @override
+  Cancellable subscribe<S extends Subscriber<dynamic, Error>>(S subscriber) {
+    // TODO: implement subscribe
     throw UnimplementedError();
   }
 }
